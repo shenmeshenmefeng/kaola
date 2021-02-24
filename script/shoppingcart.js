63bd3676d81eb9bc2532e652df612c25603de601
shenmeshenmefeng <2250332902@qq.com>
2021-02-24 19:50:28 +0800

    2.24

--- script/shoppingcart.js
+++ script/shoppingcart.js
@@ -47,3 +47,20 @@ $(function (){
     $('.list').html(newLi);
   }
 });
+
+
+$(function(){
+  var login = getCookie('login')
+  var user = getCookie('name')
+  if(login) {
+      $('.login').css('display','none')
+      $('.user').css('display','block')
+      $('.exit').css('display','block')
+      $('.user').text(user)
+  }
+})
+$('.exit').on('click',function(){
+  removeCookie('login')
+  window.location.reload()
+})
+
